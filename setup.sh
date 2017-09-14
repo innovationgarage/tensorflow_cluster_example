1237bcba5dcf5915c0e60dca11676226a996bc8e
Egil <egil.moller@freecode.no>
2017-09-14 15:40:28 +0200

    Bugfix

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ if [ "$1" == "" ]; then
     echo "Usage: setup.sh SERVER_1_IP,SERVER_2_IP,..SERVER_N_IP"
     exit
 fi
-export SERVERS="$1"
-export SERVERS="root@$(echo $SERVERS | sed -e "s+,+,root@+g")"
+export SERVER_IPS="$1"
+export SERVERS="root@$(echo $SERVER_IPS | sed -e "s+,+,root@+g")"
 
 (IFS=, ; for SERVER in $SERVERS; do ssh -oStrictHostKeyChecking=no $SERVER 'echo done'; done; ) 
 
@@ -21,4 +21,6 @@ parallel --no-notice --nonall --line-buffer -S $SERVERS 'pip install tensorflow'
     done
 ) 
 
+
+export SERVER_PORTS="$(echo $SERVER_IPS | sed -e "s+,+:4711,+g"):4711"
 parallel --no-notice --nonall --line-buffer -S $SERVERS "nohup ./trainingserver.py --hosts $SERVER_PORTS & sleep 2"
