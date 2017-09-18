b0ee1b3389bd65a1ade7b224cb25de13685b1147
Egil <egil.moller@freecode.no>
2017-09-18 12:20:32 +0200

    name support

--- trainingserver.py
+++ trainingserver.py
@@ -5,6 +5,7 @@ import sys
 
 import tensorflow as tf
 import netifaces
+import socket
 
 FLAGS = None
 
@@ -22,6 +23,7 @@ def main(_):
                    if netifaces.AF_INET in netifaces.ifaddresses(iface)])
 
   task_ips = [host.split(":")[0] for host in hosts]
+  task_ips = [socket.gethostbyname(ip) for ip in task_ips]
   local_task_ip = iter(local_ips.intersection(set(task_ips))).next()
   
   task_index = task_ips.index(local_task_ip)
